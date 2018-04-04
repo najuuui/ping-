68957e8b1f7e9f3c5b1c371edf537b55d1f229f1
jeremy-mutitu <jeremiahmutitu@gmail.com>
2018-04-04 12:27:08 +0300

    initialized a ping pong repository

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+$(function() {
+  var myArray = [];
+
+  var pingPong = function(number) {
+
+    for (var x = 1; x <= number; x++) {
+      if (x % 3 == 0 && x % 5 == 0) {
+        myArray.push('pingpong');
+      } else if (x % 3 == 0) {
+        myArray.push('ping');
+      } else if (x % 5 == 0) {
+
+        myArray.push('pong');
+      } else {
+
+        myArray.push(x);
+      };
+
+    };
+
+  };
+  console.log(myArray);
+  $('form').submit(function(event) {
+    event.preventDefault();
+
+    var num = $('#myinput').val();
+    pingPong(num);
+    myArray.forEach(function(i) {
+      $('ul').append($('<li>').html(i));
+    });
+
+  });
+
+
+
+
+});
